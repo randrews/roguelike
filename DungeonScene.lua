ba5614ecedd550c006b43c159655283cb69e3658
Ross Andrews <ross.andrews@gmail.com>
2013-07-12 19:44:37 -0500

    Scrolling when going between rooms

--- DungeonScene.lua
+++ DungeonScene.lua
@@ -14,6 +14,7 @@ local tiles = {
 }
 
 function DungeonScene:initialize(dungeon)
+    self.frozen = false -- if frozen, ignore kbd input
     sonnet.Scene.initialize(self)
     self.dungeon = dungeon
     self.player = {}
@@ -67,7 +68,44 @@ function DungeonScene:setQuads(room)
 end
 
 function DungeonScene:draw()
-    for pt, cell in self.room:each() do
+    if self.scroll then
+        local w, h = self.room.width*32, self.room.height*48
+        local v = self.scroll.tween.value
+        local new = nil -- where to draw the new room
+        local old = nil -- where to draw the old room
+
+        if self.scroll.dir == Point.south then
+            new = Point(0, h*v)
+            old = Point(0, h*v-h)
+        elseif self.scroll.dir == Point.north then
+            new = Point(0, h*v*-1)
+            old = Point(0, h*v*-1+h)
+        elseif self.scroll.dir == Point.east then
+            new = Point(w*v, 0)
+            old = Point(w*v-w, 0)
+        elseif self.scroll.dir == Point.west then
+            new = Point(w*v*-1, 0)
+            old = Point(w*v*-1+w, 0)
+        end
+
+        love.graphics.push()
+        love.graphics.translate(new())
+        self:drawRoom(self.room)
+        love.graphics.pop()
+        love.graphics.push()
+        love.graphics.translate(old())
+        self:drawRoom(self.scroll.old_room)
+        love.graphics.pop()
+        
+    else
+        self:drawRoom(self.room)
+    end
+
+    self:fps()
+end
+
+function DungeonScene:drawRoom(room)
+    for pt, cell in room:each() do
         local x, y = pt.x * 32, pt.y * 48
 
         if cell.terrain_quad then
@@ -82,10 +120,11 @@ function DungeonScene:draw()
 
     end
 
-    love.graphics.drawq(tilesheet, tiles.player,
-                        self.player.location.x*32,
-                        self.player.location.y*48)
-    self:fps()
+    if room == self.room then -- draw player
+        love.graphics.drawq(tilesheet, tiles.player,
+                            self.player.location.x*32,
+                            self.player.location.y*48)
+    end
 end
 
 function DungeonScene:keypressed(key)
@@ -93,6 +132,8 @@ function DungeonScene:keypressed(key)
         love.event.quit()
         return
     end
+
+    if self.frozen then return end
 
     local dir = Point.from_key(key)
     if dir then
@@ -158,6 +199,7 @@ function DungeonScene:changeRooms(dir)
         self.player.location.x = self.room.width-1
     end
 
+    local old_room = self.room
     self:setRoom(new_room)
 
     -- Often, the player will be standing on
@@ -165,4 +207,21 @@ function DungeonScene:changeRooms(dir)
     -- if so.
     local obj = self:getSolid(self.player.location)
     if obj and obj.type == 'door' then self:openDoor(obj) end
+
+    -- Animate the transition
+    -- This has a tween that we'll multiply the direction by,
+    -- to get the place to draw the new room
+    self.scroll = {
+        tween = sonnet.Tween(1, 0, 0.3),
+        dir = dir,
+        old_room = old_room
+    }
+
+    self.frozen = true -- Freeze while scrolling
+
+    -- Unfreeze when done scrolling
+    self.scroll.tween:promise():add(function()
+                                        self.frozen = false
+                                        self.scroll = nil
+                                    end)
 end
