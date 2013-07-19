5a2969daf5e376e7aef0834988cf29969f292cde
Ross Andrews <ross.andrews@gmail.com>
2013-07-18 22:42:26 -0500

    Skin stuff

--- RogueSkin.lua
+++ RogueSkin.lua
@@ -1,4 +1,14 @@
-local skin = loveframes.util.GetActiveSkin()
+local blue = loveframes.skins.available.Blue
+
+local skin = {}
+
+skin.controls = loveframes.util.Copy(blue.controls)
+skin.OnRegister = blue.OnRegister
+
+skin.name = 'Rogue'
+skin.author = 'Ross Andrews'
+skin.version = '1.0'
+skin.directory = 'loveframes/skins/Blue'
 
 skin.controls.frame_name_font = GameFont
 skin.controls.button_text_font = GameFont
@@ -13,3 +23,7 @@ skin.controls.columnlistheader_text_font = GameFont
 local bg = {46, 40, 49, 255}
 skin.controls.panel_body_color = bg
 skin.controls.tabpanel_body_color = bg
+
+loveframes.skins.Register(skin)
+
+loveframes.config.ACTIVESKIN = 'Orange'
